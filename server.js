893e77472e41574dd0b54d0de10748f98a487631
Cristine <kriszta.eross@gmail.com>
2023-08-27 15:25:28 +0300

    one route and get method added, hanging resolved

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const http = require('http')
 const data = require('./data/data.json')
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.writeHead(200, {'Content-Type': 'application/json'})
-    res.end(JSON.stringify(data))
+    if(req.url === '/api/questions' && req.method === 'GET') {
+        res.writeHead(200, {'Content-Type': 'application/json'})
+        res.end(JSON.stringify(data))
+    } else {
+        res.writeHead(404, {'Content-Type': 'application/json'})
+        res.end(JSON.stringify({ message: 'Route not found' }))
+    }
 })
 const PORT = process.env.PORT || 5000
 
